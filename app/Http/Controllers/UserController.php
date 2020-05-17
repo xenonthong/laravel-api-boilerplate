7ccd08f4a8b40d9d88c244b63ae2b5be74ee25a9
allaniftrue <ajmcagadas@gmail.com>
2020-05-17 12:56:50 +0800

    Add address to fillable, remove unused methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,16 +20,6 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -51,17 +41,6 @@ class UserController extends Controller
         return response()->json([
             'user' => \Auth::user()
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'username', 'mobile'
+        'name', 'email', 'password', 'username', 'mobile', 'address'
     ];
 
     /**
